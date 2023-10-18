6aa03887ab6eff17a92bbdbe0610d363d9ff04ef
zhuchentong <2037630@163.com>
2023-10-18 13:17:21 +0800

    release(patch): fixed defineForms type

--- packages/dynamic-table/src/defines/define-forms.ts
+++ packages/dynamic-table/src/defines/define-forms.ts
@@ -1,5 +1,5 @@
 import type { FormItemsOptions } from '@/interfaces'
 
-export function defineForms(forms: FormItemsOptions[]) {
+export function defineForms(forms: FormItemsOptions) {
   return forms
 }
